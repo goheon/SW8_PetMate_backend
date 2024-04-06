6b4fbad2b6f6e624c0bf7ef6dd0be863e4309ee6
전다현 <jigunt9014@gmail.com>
2024-04-06 20:02:06 +0900

    feat: 회원정보조회 res json형식 수정

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,15 @@ userRouter.get('/:userId', async (req,res)=>{
     try{
         const userId = req.params.userId
         const searchUser=await userService.getUserInfo(userId);
-        console.log(searchUser)
-        res.status(200).json({message:"유저 정보 조회 성공" ,data:searchUser})
+
+        res.status(200).json({
+            username:searchUser.username,
+            email: searchUser.email,
+            password: searchUser.password,
+            phone: searchUser.phone,
+            address: searchUser.address,
+            detailAddress: searchUser.password,
+        })
         }catch(error){
             console.error("정보 조회 중 오류 발생",error);
             res.status(500).json({message:"서버오류"});
