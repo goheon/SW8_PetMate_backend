c0babb21f7af88248129d9967879247f01ece787
전다현 <jigunt9014@gmail.com>
2024-04-06 21:22:43 +0900

    feat: 라우트 오류처리 수정

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,7 +1,7 @@
 export default (err,req,res,next)=>{
     const statusCode = err.statusCode || 500 //오류에 stautscode없으면 500
     res.status(500).json({
-        statusCode: res.statusCode,
+        statusCode: statusCode,
         errMessage:err.message
 })
 }
