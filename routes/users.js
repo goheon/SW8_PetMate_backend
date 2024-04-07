523c30d2c4dc3e3c8bc77fdf41d6ccf860b6efc8
2hani <155037923+2hani@users.noreply.github.com>
2024-04-08 02:41:58 +0900

    feat:회원정보수정및 탈퇴 api작성

--- routes/users.js
+++ routes/users.js
@@ -5,24 +5,50 @@ import { tokenAuthenticated } from '../middlewares/tokenMiddleware.js';
 export const userRouter = express.Router();
 
 //회원 정보 조회
-userRouter.get('/:userId',tokenAuthenticated, async (req,res,next)=>{
-    try{
+userRouter.get('/:userId', tokenAuthenticated, async (req, res, next) => {
+    try {
         const userId = req.params.userId
-        const searchUser=await userService.getUserInfo(userId);
+        const searchUser = await userService.getUserInfo(userId);
 
         res.status(200).json({
-            username:searchUser.username,
+            username: searchUser.username,
             email: searchUser.email,
             password: searchUser.password,
             phone: searchUser.phone,
             address: searchUser.address,
             detailAddress: searchUser.detailAddress,
         })
-        }catch (error) {
-            next(error); // 에러를 다음 미들웨어로 전달
-        }
+    } catch (error) {
+        next(error); // 에러를 다음 미들웨어로 전달
+    }
 })
 
 //회원 정보 수정
+userRouter.put('/:userId', tokenAuthenticated, async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        const userInfo = req.body;
+        const updatedUser = await userService.updateUserInfo(userId, userInfo);
+
+        res.status(200).json({
+            message: '회원 정보가 수정되었습니다.',
+            data: updatedUser
+        });
+    } catch (error) {
+        next(error);
+    }
+});
 
 //회원 탈퇴
+userRouter.delete('/:userId', tokenAuthenticated, async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        await userService.deleteUser(userId);
+
+        res.status(200).json({
+            message: '회원 탈퇴가 완료되었습니다.'
+        });
+    } catch (error) {
+        next(error);
+    }
+});
