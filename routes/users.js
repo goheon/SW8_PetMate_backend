f919bfa159bf50a477d705103543eefa7003d5f8
전다현 <jigunt9014@gmail.com>
2024-04-08 16:25:50 +0900

    feat: 회원정보 조회 res 수정

--- routes/users.js
+++ routes/users.js
@@ -10,14 +10,7 @@ userRouter.get('/:userId', tokenAuthenticated, async (req, res, next) => {
         const userId = req.params.userId
         const searchUser = await userService.getUserInfo(userId);
 
-        res.status(200).json({
-            username: searchUser.username,
-            email: searchUser.email,
-            password: searchUser.password,
-            phone: searchUser.phone,
-            address: searchUser.address,
-            detailAddress: searchUser.detailAddress,
-        })
+        res.status(200).json(searchUser);
     } catch (error) {
         next(error); // 에러를 다음 미들웨어로 전달
     }
