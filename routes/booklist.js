1ece3684f0a1dbaa75c2fbb519cc0c33ca9382fd
이고헌 <gohoney22@gmail.com>
2024-04-12 17:52:44 +0900

    Fix: booklist.js 내 예약 내역 조회 API 수정
    일반 사용자 예약 내역 조회시
    사용자의 각각의 주문 내역 내에
    주문 건의 펫시터 정보, 유저 정보 추가

--- routes/booklist.js
+++ routes/booklist.js
@@ -1,6 +1,8 @@
 import express from 'express';
 // import reviewService from "../services/reviewService.js";
-import orderService from "../services/orderService.js";
+import orderService from '../services/orderService.js';
+import petsitterService from '../services/petsitterService.js';
+import userService from '../services/userService.js';
 import { tokenAuthenticated } from '../middlewares/tokenMiddleware.js';
 
 export const booklistRouter = express.Router();
@@ -10,16 +12,24 @@ booklistRouter.get('/', tokenAuthenticated, async (req, res, next) => {
   try {
     const userId = req.cookies.jwt;
     const orders = await orderService.getOrderListOfUser(userId);
-    console.log(orders);
 
-    if (orders === null) {
+    const ordersWithSitterInfo = await Promise.all(
+      orders.map(async (order) => {
+        const petSitterInfo = await petsitterService.getPetSitterById(order.sitterId);
+        const userInfo = await userService.getUserInfo(req.cookies.jwt);
+        const orderObj = order.toObject();
+        return { ...orderObj, petSitterInfo, userInfo };
+      }),
+    );
+
+    if (ordersWithSitterInfo === null) {
       return res.status(404).json({ message: '전체 예약 내역이 없습니다.' });
     } else {
       res.status(200).json({
         message: '전체 예약 내역 조회가 완료되었습니다.',
-        data: orders
+        data: ordersWithSitterInfo,
       });
-    };
+    }
   } catch (error) {
     next(error);
   }
