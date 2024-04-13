ad837b38e5ce57fa1b09e971dc1a3ed6b0fd636f
이고헌 <gohoney22@gmail.com>
2024-04-14 00:04:21 +0900

    Fix: petsitterSchema 수정
    펫시터 스키마의 시간당 비용 스키마 Object로 수정

--- db/schemas/petsitterSchema.js
+++ db/schemas/petsitterSchema.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import shortId from './types/shortId.js'; 
+import shortId from './types/shortId.js';
 const { Schema } = mongoose;
 
 const petsitterSchema = new Schema({
@@ -12,9 +12,9 @@ const petsitterSchema = new Schema({
     type: String,
     required: true,
   },
-  phone:{
-    type:Number,
-    required:true,
+  phone: {
+    type: Number,
+    required: true,
   },
   title: {
     type: String,
@@ -28,18 +28,18 @@ const petsitterSchema = new Schema({
     type: String,
     required: true,
   },
-  hourlyRate: [{
-    size:{type: String, required:true},
-    rate:{type:Number, required:true}
-  }],
+  hourlyRate: {
+    type: Object,
+    required: true,
+  },
   image: {
     type: [String],
-    default: "public/images/default.png"
+    default: 'public/images/default.png',
   },
   type: {
     type: [String],
     required: true,
-  }
-})
+  },
+});
 
 export default petsitterSchema;
