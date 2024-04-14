2f3efe3113d3c0fb0956d6be1b6a86aeb1bcded6
이고헌 <gohoney22@gmail.com>
2024-04-14 21:48:24 +0900

    Fix: 유저 스키마, 리뷰 스키마 수정 +
    유저 스키마: phone : string 변경
    리뷰 스키마: title 필드 추가
    리뷰 추가 API: create 내 title 추가, img 파일 업로드 분기 추가

--- routes/review.js
+++ routes/review.js
@@ -1,22 +1,26 @@
 import express from 'express';
-import ReviewService from '../services/reviewService.js'
+import ReviewService from '../services/reviewService.js';
 import { tokenAuthenticated } from '../middlewares/tokenMiddleware.js';
 import { uploadFiles } from '../middlewares/imageMiddleware.js';
 
-export const reviewRouter=express.Router();
+export const reviewRouter = express.Router();
 const reviewService = new ReviewService();
 
 // 리뷰 작성
-reviewRouter.post('/:orderId', tokenAuthenticated, uploadFiles.fields([{ name: 'img', maxCount: 3 }]), async (req, res, next) => {
-  try {
-    const orderId=req.params.orderId;
-    const uploadFiles = req.files['img'];
-    const uploadimg = uploadFiles ? uploadFiles.map(file => file.path) : [];
-    await reviewService.writeReview(orderId, req.body, uploadimg);
+reviewRouter.post(
+  '/:orderId',
+  tokenAuthenticated,
+  uploadFiles.fields([{ name: 'img', maxCount: 3 }]),
+  async (req, res, next) => {
+    try {
+      const orderId = req.params.orderId;
+      const uploadFiles = req.files;
+      const uploadimg = uploadFiles['img'] ? uploadFiles['img'].map((file) => file.path) : [];
+      await reviewService.writeReview(orderId, req.body, uploadimg);
 
-    res.status(200).send("리뷰 작성 완료!");
-
-  }catch(error){
-    next(error);
-  }
-})
+      res.status(200).send('리뷰 작성 완료!');
+    } catch (error) {
+      next(error);
+    }
+  },
+);
--- services/reviewService.js
+++ services/reviewService.js
@@ -1,42 +1,42 @@
 // 불러오기
-import { Review } from "../db/index.js";
-import { Order } from "../db/index.js";
-import { User } from "../db/index.js";
+import { Review } from '../db/index.js';
+import { Order } from '../db/index.js';
+import { User } from '../db/index.js';
 
 class ReviewService {
   constructor() {
     this.Review = Review;
-    this.Order= Order;
-    this.User=User;
+    this.Order = Order;
+    this.User = User;
   }
 
   // 이용후기 작성
   async writeReview(orderId, reviewinfo, images) {
     const reviewId = await this.Order.findOne({ orderId: orderId });
 
-    const { comment, starRate } = reviewinfo;
+    const { comment, starRate, title } = reviewinfo;
 
-    if (starRate < 0 || starRate > 6){
-      throw new Error("1부터 5사이의 점수만 입력할 수 있습니다.")
+    if (starRate < 0 || starRate > 6) {
+      throw new Error('1부터 5사이의 점수만 입력할 수 있습니다.');
     }
 
     const searchuserId = reviewId.userId;
     const searchsitterId = reviewId.sitterId;
 
-    const searchname= await this.User.findOne({userId : searchuserId});
-    const username=searchname.username
-  
+    const searchname = await this.User.findOne({ userId: searchuserId });
+    const username = searchname.username;
+
     return await this.Review.create({
-        orderId,
-        userId: searchuserId,
-        sitterId: searchsitterId,
-        username: username,
-        comment,
-        image: images,
-        starRate
-      });
+      orderId,
+      userId: searchuserId,
+      sitterId: searchsitterId,
+      username: username,
+      title,
+      comment,
+      image: images,
+      starRate,
+    });
   }
-  
 
   // 전체 이용후기 목록 조회
   async getReviewList() {
@@ -48,7 +48,7 @@ class ReviewService {
     return await this.Review.findOneAndUpdate(
       { _id: reviewId },
       { $push: { comments: { sitter_id: sitterId, comment: comment } } },
-      { new: true }
+      { new: true },
     );
   }
 
