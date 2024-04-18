a9f1ae825563338e4c9471d90863f0618ccf5e89
이고헌 <gohoney22@gmail.com>
2024-04-18 12:14:00 +0900

    Fix: 펫시터 전환 시 default 이미지 변경

--- routes/users.js
+++ routes/users.js
@@ -64,7 +64,9 @@ userRouter.post(
   async (req, res, next) => {
     try {
       const uploadFiles = req.files['img'];
-      const uploadimg = uploadFiles ? uploadFiles.map((file) => file.location) : ['public/images/default.png'];
+      const uploadimg = uploadFiles
+        ? uploadFiles.map((file) => file.location)
+        : ['https://elice-project2-pet-mate.s3.ap-northeast-2.amazonaws.com/contents/sitter-default.jpg'];
       const result = await userService.registerSitter(req.userId, req.body, uploadimg);
 
       if (result.success) {
