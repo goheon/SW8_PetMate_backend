7e34212fce7f15ae5463d2c8b9354f50fa4fa6ef
이고헌 <gohoney22@gmail.com>
2024-04-20 03:22:21 +0900

    Fix: 토큰 httpOnly 옵션 false 수정

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,15 +1,14 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-export const setUserToken=(res,user)=>{
-    //DB데이터 가져옴
-    const { userId }=user;
-    const secret=process.env.SECRET_KEY;
+export const setUserToken = (res, user) => {
+  //DB데이터 가져옴
+  const { userId } = user;
+  const secret = process.env.SECRET_KEY;
 
-    //user정보를 가지고 토큰 생성
-    const token=jwt.sign({userId},secret)
+  //user정보를 가지고 토큰 생성
+  const token = jwt.sign({ userId }, secret);
 
-    //토큰을 쿠키로 전달
-    res.cookie('jwt', token, { httpOnly: true });
-    res.send("로그인 성공!");
-} 
-
+  //토큰을 쿠키로 전달
+  res.cookie('jwt', token, { httpOnly: false });
+  res.send('로그인 성공!');
+};
